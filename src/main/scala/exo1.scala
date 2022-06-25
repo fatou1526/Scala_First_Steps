bc0a88fabc08b72a45199f522f94696ba7a30304
Ndeye Fatou <ndeyefatousene1502@gmail.com>
2022-06-25 12:55:05 +0000

    variables exercises

--- src/main/scala/exo1.scala
+++ src/main/scala/exo1.scala
@@ -1,5 +1,18 @@
 object exo1 extends App{
-  println("hello world")
-
+  /*
+  Créer une variable mutable nommée « profession » et renseigner la
+valeur : Data Engineer
+  Vous avez été muté et vous êtes maintenant « scrum master ». Changer
+votre profession.
+  Créer en une seule commande les variable nom, prenom et age. Pour des
+raisons de fraudes sur l’identité de la personnes, personne ne doit
+pouvoir modifier les valeurs de ces variables.
+  Afficher les variables à l’aide de la fonction println(" ")
+  */
+  var profession : String = "Data Engineer"
+  profession = "Scrum Master"
+  val (nom, prenom, age) = ("diop", "astou", 23)
+  println(s"Mon age est $age")
+  println("Et mon nom complet est %s %s".format(nom,prenom))
 
 }
