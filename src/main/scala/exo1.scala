092cb986082719a8a3362bef996f64ba8fc4aabf
Ndeye Fatou <ndeyefatousene1502@gmail.com>
2022-07-07 12:31:13 +0000

    some exercices with operators

--- src/main/scala/exo1.scala
+++ src/main/scala/exo1.scala
@@ -65,6 +65,35 @@ Combien de temps (en Jours et en Heure) il faut à un marcheur pour parcourir un
   val jour: Int = (temps/24).toInt
   val heure = temps-(jour*24)
   println(s"Il va marcher $jour jours et $heure heures")
-  */
+
+  Un magicien dit que
+  • Quand on choisit un nombre premier différent de 2 et 3
+  • On l’èleve au carré
+  • On lui ajoute 17
+  • On divise par 12
+  • Alors le reste de la division vaut 6 ?
+
+
+  val nbPremier = 17
+  val isMagicianRight = ((nbPremier * nbPremier) + 17) % 12 == 6
+  println(s"Est ce que le magicien a raison pour le nombre $nbPremier? $isMagicianRight")
+
+
+ Le gouvernement a décidé d'offrir une prime de 300€ à certains fonctionnaires en fonction de leur salaire et de leur ancienneté.
+Comme toutes les autres mesures prises par le gouvernement, il est difficile de comprendre à qui cette mesure s'applique.
+De ce que vous avez compris, une personne peut toucher à la prime si :
+Critère 1 : Elle a de d'ancienneté son salaire est à euros.
+Critère 2 : Elle a d'ancienneté son salaire est compris euros.
+Critère 3 : Elle a de d'ancienneté son salaire est strictement à euros à euros. C'est à
+dire qu'une personne ayant plus de 10 ans d'ancienneté et un salaire entre 1500 et 2300 euros ne peut pas toucher à cette prime.
+Bernadette a ans d'ancienneté et un salaire de euros.
+Marc a ans d'ancienneté et un salaire de euros.
+ */
+  val employe = "Bernadette"
+  val anciennete = 12
+  val salaire = 2400
+  val isPrime = (anciennete<5 && salaire<1500)||((anciennete>5 && anciennete<10) && (salaire>=1500 && salaire<2300)) ||(anciennete>=10 && (salaire<1500 || salaire>2300))
+  println(s"Est ce que $employe sera primé? $isPrime")
+
 
 }
